c0bc25d1fe4efe8a811f3c8782c969a0d0d9ca66
deamyasin <73380813+deamyasin@users.noreply.github.com>
2021-06-17 21:06:11 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,6 @@
 <?php
-// Membuat variabel, ubah sesuai dengan nama host dan database pada hosting 
-$host	= "localhost";
-$user	= "root";
-$pass	= "";
-$db	= "mahasiswa";
-
-//Menggunakan objek mysqli untuk membuat koneksi dan menyimpan nya dalam variabel $mysqli	
+$host    = "localhost";
+$user    = "root";
+$pass    = "";
+$db    = "mahasiswa";
 $mysqli = new mysqli($host, $user, $pass, $db);
